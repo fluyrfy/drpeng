9d02662e684cad6d809f038b66a7f64e844b401d
Hao1229 <s0939859391@gmail.com>
2020-12-04 15:19:43 +0800

    feat: add chapter detail section

---
+++ components/subject/chapterList.vue
@@ -0,0 +1,199 @@
+<template>
+  <div class="chapter-list">
+    <section
+      v-for="(item, index) in subjectList"
+      :key="index"
+      class="chapter-list__list-section"
+    >
+      <div
+        class="d-flex chapter-list__list-group"
+        @click="onChapterClick(item.id)"
+      >
+        <div class="chapter-list__list-group--list-chapter">
+          <span>
+            CH0{{ item.id }}
+          </span>
+        </div>
+        <div class="chapter-list__list-group--list-title d-flex align-center">
+          <span>
+            {{ item.name }}
+          </span>
+          <div
+            v-if="activeChapter.indexOf(item.id) > -1"
+            class="chapter-list__list-group--fold-button"
+          />
+          <div
+            v-else
+            class="chapter-list__list-group--unfold-button"
+          />
+        </div>
+      </div>
+      <div
+        class="chapter-list__list-group--detail"
+        :class="{'chapter-list__list-group--detail-active': activeChapter.indexOf(item.id) > -1}"
+      >
+        <ul>
+          <li>
+            <img src="~/assets/img/icon/chapter-more-icon.svg">
+            <span class="chapter-list__detail-num">
+              1.1
+            </span>
+            <span>
+              Four Ways to Represent a Function
+            </span>
+          </li>
+          <li>
+            <img src="~/assets/img/icon/chapter-more-icon.svg">
+            <span class="chapter-list__detail-num">
+              1.2
+            </span>
+            <span>
+              Mathematical Models: A Catalog of Essential Functions
+            </span>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    subjectList: {
+      type: Array,
+      default: () => [
+        {
+          id: 1,
+          subject: 1,
+          name: 'Functions and Limits',
+          status: true
+        },
+        {
+          id: 2,
+          subject: 1,
+          name: 'Derivatives',
+          status: true
+        },
+        {
+          id: 3,
+          subject: 1,
+          name: 'Applications of Differentiation',
+          status: true
+        }
+      ]
+    }
+  },
+  data () {
+    return {
+      activeChapter: []
+    }
+  },
+  methods: {
+    onChapterClick (id) {
+      const index = this.activeChapter.indexOf(id)
+
+      if (index > -1) {
+        this.activeChapter.splice(index, 1)
+      } else {
+        this.activeChapter.push(id)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.chapter-list {
+  padding: 0 3.47%;
+  margin-top: 36px;
+  &__list-group {
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+    &--list-chapter {
+      flex: 0 0 20.57%;
+      background: #c7c7c7;
+      color: white;
+      padding: 17px 12px 15px 14px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      span {
+        font-weight: bold;
+        font-size: 18px;
+        line-height: 1.06;
+      }
+    }
+
+    &--list-title {
+      flex: auto;
+      padding-left: 14px;
+      background: #fffef4;
+
+      span {
+        font-size: 20px;
+        color: #747474;
+        line-height: 1.5;
+        font-weight: bold;
+        max-width: calc(100% - 50px);
+      }
+    }
+
+    &--unfold-button {
+      width: 50px;
+      height: 50px;
+      background-image: url('~assets/img/icon/unfold-icon.svg');
+      background-size: cover;
+      background-position: center center;
+      background-repeat: no-repeat;
+      margin-left: auto;
+      cursor: pointer;
+    }
+
+    &--fold-button {
+      width: 50px;
+      height: 50px;
+      background-image: url('~assets/img/icon/fold-icon.svg');
+      background-size: cover;
+      background-position: center center;
+      background-repeat: no-repeat;
+      margin-left: auto;
+      cursor: pointer;
+    }
+
+    &--detail {
+      color: #8e8e8e;
+      line-height: 1.38;
+      display: flex;
+      margin-top: 6px;
+      background: #fffef4;
+      height: 0;
+      overflow: hidden;
+
+      img {
+        height: 20px;
+      }
+
+      ul {
+        padding-left: 0;
+        width: 100%;
+      }
+
+      li {
+        display: flex;
+        box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+        padding: 15px 10px;
+      }
+    }
+
+    &--detail-active {
+      height: auto;
+      margin-bottom: 14px;
+    }
+  }
+
+  &__detail-num {
+    margin: 0 13px;
+  }
+}
+</style>
--- pages/subject/_category/index.vue
+++ pages/subject/_category/index.vue
@@ -18,25 +18,16 @@
         Calculus
       </h2>
     </section>
-    <section class="subject__list-section">
-      <div class="d-flex subject__list-group">
-        <div class="subject__list-group--list-chapter">
-          <span>
-            CH01
-          </span>
-        </div>
-        <div class="subject__list-group--list-title d-flex align-center">
-          <span>
-            Functions and Limits
-          </span>
-        </div>
-      </div>
-    </section>
+    <chapter-list />
   </div>
 </template>
 
 <script>
+import chapterList from '~/components/subject/chapterList'
 export default {
+  components: {
+    'chapter-list': chapterList
+  },
   data () {
     return {
     }
@@ -50,6 +41,7 @@ export default {
 .subject {
   background:#FCF7CE;
   min-height: 100vh;
+  padding-bottom: 22px;
 
   &__title-section {
     padding-top: 29px;
@@ -70,41 +62,6 @@ export default {
     img {
       height: 100%;
       width: 100%;
-    }
-  }
-
-  &__list-section {
-    padding: 0 3.47%;
-    margin-top: 36px;
-  }
-
-  &__list-group {
-    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
-    &--list-chapter {
-      flex: 0 0 20.57%;
-      text-align: center;
-      background: #c7c7c7;
-      color: white;
-      padding: 17px 12px 15px 14px;
-
-      span {
-        font-weight: bold;
-        font-size: 18px;
-        line-height: 1.06;
-      }
-    }
-
-    &--list-title {
-      flex: auto;
-      padding-left: 14px;
-      background: #fffef4;
-
-      span {
-        font-size: 20px;
-        color: #747474;
-        line-height: 0.95;
-        font-weight: bold;
-      }
     }
   }
 }
