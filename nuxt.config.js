0bbf8af9aa3f8b100a0ff9310a01abbd1ede5870
Hao1229 <s0939859391@gmail.com>
2021-02-20 14:12:01 +0800

    feat: modify config to disable user scale

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     title: '彭博士知識地圖',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
